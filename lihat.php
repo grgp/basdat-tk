cd37fe4053af5b7aa9db85f587087091db1f32df
grgp <g.a.pitoy@gmail.com>
2016-05-13 14:33:20 +0700

    first commit, lihat

---
+++ lihat.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>SILUTEL - </title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!--
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		-->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flat-ui/2.3.0/css/flat-ui.css">
+
+		<link href='https://fonts.googleapis.com/css?family=Lato:400,700,300,900' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<h3>Whhaaa</h3>
+					<p>Oh yeeeafafnknknkn</p>
+				</div>
+			</div>
+		</div>
+	</body>
+
+</html>
