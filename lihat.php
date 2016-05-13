26fc8bb1c84c105111ffbba04c5f920927d3f571
grgp <g.a.pitoy@gmail.com>
2016-05-13 14:52:58 +0700

    things

--- lihat.php
+++ lihat.php
@@ -4,10 +4,9 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<!--
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-		-->
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flat-ui/2.3.0/css/flat-ui.css">
+		<link rel="stylesheet" href="style-g.css">
+		<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flat-ui/2.3.0/css/flat-ui.css"> -->
 
 		<link href='https://fonts.googleapis.com/css?family=Lato:400,700,300,900' rel='stylesheet' type='text/css'>
 		<link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
@@ -16,13 +15,64 @@
 
 	<body>
 		<div class="container">
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<h3>Whhaaa</h3>
-					<p>Oh yeeeafafnknknkn</p>
-				</div>
+			<div class="row">
+				<div class="col-md-8 col-md-offset-4">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="row">
+								<h3 class="text-center">SILUTEL - BOOKING</h3>
+
+								<div class="btn-group" role="group" aria-label="...">
+								  <div class="btn-group" role="group">
+								    <button type="button" class="btn btn-default">Invoice</button>
+								  </div>
+								  <div class="btn-group" role="group">
+								    <button type="button" class="btn btn-default">Tanggal Datang</button>
+								  </div>
+								  <div class="btn-group" role="group">
+								    <button type="button" class="btn btn-default">Tanggal Pergi</button>
+								  </div>
+								</div>
+
+								<button type="button" class="btn btn-default btn-ascdesc">Left</button>
+							</div>
+							<div class="row">
+								<table>
+									<th></th>
+									<tr>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+									</tr>
+									<tr>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+									</tr>
+									<tr>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+									</tr>
+								</table>
+							</div>
+						</div> <!-- end panel-body -->
+					</div> <!-- end panel -->
+				</div> <!-- end col -->
 			</div>
-		</div>
+		</div> <!-- end container -->
 	</body>
 
 </html>
