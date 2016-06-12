3f169e01950f8c9d6ae7681f88798752faf74b6c
grgp <g.a.pitoy@gmail.com>
2016-06-12 22:50:12 +0700

    modified connect.php to connect to dbpgcsui

--- db/connect.php
+++ db/connect.php
@@ -1,6 +1,6 @@
 <?php
 	function queryDB($query) {
-		$conn = pg_connect("user=postgres dbname=postgres");
+		$conn = pg_connect("host=dbpg.cs.ui.ac.id port=5432 user=d207 dbname=d207 pass=bdd0722015");
 
 		if (!$conn) {
 		  echo "An error occurred.\n";
