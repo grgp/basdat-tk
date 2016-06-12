d620ed3dc5c9956aea7d93f391987e11f48ffad2
grgp <g.a.pitoy@gmail.com>
2016-06-13 02:47:30 +0700

    updated connect.php to work

--- db/connect.php
+++ db/connect.php
@@ -1,9 +1,10 @@
 <?php
 	function queryDB($query) {
-		$conn = pg_connect("host=dbpg.cs.ui.ac.id port=5432 user=d207 dbname=d207 pass=bdd0722015");
+		$conn = pg_connect("host=dbpg.cs.ui.ac.id port=5432 dbname=d207 user=d207 password=bdd0722015");
+		// $conn = pg_connect("user=postgres dbname=postgres");
 
 		if (!$conn) {
-		  echo "An error occurred.\n";
+		  echo "An error occurred. Connection cannot be established.\n";
 		  exit;
 		}
 
