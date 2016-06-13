7e3759f5b48b3097b7aae0192b78b26ca8fb3e89
grgp <g.a.pitoy@gmail.com>
2016-06-13 10:14:14 +0700

    sql final, probably some unwanted changes

--- db/connect.php
+++ db/connect.php
@@ -1,7 +1,7 @@
 <?php
 	function queryDB($query) {
-		$conn = pg_connect("host=dbpg.cs.ui.ac.id port=5432 dbname=d207 user=d207 password=bdd0722015");
-		// $conn = pg_connect("user=postgres dbname=postgres");
+		//$conn = pg_connect("host=dbpg.cs.ui.ac.id port=5432 dbname=d207 user=d207 password=bdd0722015");
+		$conn = pg_connect("user=postgres dbname=postgres");
 
 		if (!$conn) {
 		  echo "An error occurred. Connection cannot be established.\n";
